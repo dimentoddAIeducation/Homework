e0b48f1df72d4fbd34a28fc056ff2433970f225e
dimentodd (Бельский Дмитрий) <dimentodd@gmail.com>
2026-02-02 23:03:54 +0700

    change name in homework

--- scratch_3.py
+++ scratch_3.py
@@ -1,8 +1,9 @@
 def say_hi():
-    print("Hello, my name is DIMMENTODD")
+    print("Hello, my name is DIMENTODD")
 
 def make_protected_callback(password, callback):
         return lambda input_password: callback if input_password==password else print("no")
 
 protected= make_protected_callback("22", say_hi())
+
 protected("222")
